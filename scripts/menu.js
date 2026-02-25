1a97a53e422b3e7a60ef394fedc88defe23c27e5
JuanPablo <juanpablodiaz2518@gmail.com>
2026-02-24 21:57:35 -0300

    Logica del menu finalizada

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,26 @@
 const usuario = localStorage.getItem("usuario");
+const contenido = document.getElementById("contenido");
+const botonConsulta = document.getElementById("botonConsulta");
+const botonIngresar = document.getElementById("botonIngresar");
+const botonModificar = document.getElementById("botonModificar");
+
+async function cargarPagina(pagina) {
+  const respuesta = await fetch(pagina);
+  const html = await respuesta.text();
+  contenido.innerHTML = html;
+}
+
+botonConsulta.addEventListener("click", () => {
+  cargarPagina("verMascotas.html");
+});
+
+botonIngresar.addEventListener("click", () => {
+  cargarPagina("ingresarMascotas.html");
+});
+
+botonModificar.addEventListener("click", () => {
+  cargarPagina("modificarMascotas.html");
+});
 
 const textoBienvenida = document.getElementById("textoBienvenida");
 
